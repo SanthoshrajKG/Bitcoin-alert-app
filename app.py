8adbb4b54ccb1c5add93e277576b5509adc0b575
unknown <santhosh.gvelu@gmail.com>
2022-08-13 20:34:33 +0530

    adding files

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask, request, render_template
+from flask_mysqldb import MySQL
+import yaml
+
+app=Flask(__name__)
+mysql=MySQL(app)
+
+db=yaml.safe_load(open('db.yaml'))
+app.config['MYSQL_HOST']=db['mysql_host']
+app.config['MYSQL_USER']=db['mysql_user']
+app.config['MYSQL_PASSWORD']=db['mysql_password']
+app.config['MYSQL_DB']=db['mysql_db']
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    return 'This is my Krypto task for the infra role'
+
+@app.route('/alerts/create',methods=['GET','POST'])
+def Create_Alert():
+    if(request.method=='POST'):
+        userDetails=request.form
+        name=userDetails['name']
+        email=userDetails['emailid']
+        triggerValue=userDetails['triggervalue']
+        cur=mysql.connection.cursor()
+        cur.execute('INSERT INTO users(name,email,triggervalue,isTriggered) VALUES(%s,%s,%s,%s)',(name,email,triggerValue,'NO'))
+        mysql.connection.commit()
+        cur.close()
+        return 'Created an Alert'
+    return render_template('index.html')
+
+@app.route('/alerts/delete/',methods=['GET','POST'])
+def Deleting_alert():
+    if(request.method=='POST'):
+        userDetails=request.form
+        name=userDetails['name']
+        cur=mysql.connection.cursor()
+        cur.execute('DELETE FROM users WHERE name="%s"'%name)
+        mysql.connection.commit()
+        cur.close()
+        return 'Deleted an Alert'
+    return render_template('del.html')
+
+@app.route('/alerts/fetch_all',methods=['GET'])
+def Fetching_all_alert():
+    cur=mysql.connection.cursor()
+    rows=cur.execute('SELECT * FROM users')
+    if(rows>0):
+        userDetails=cur.fetchall()
+        cur.close()
+        return render_template('triggers.html',userDetails=userDetails)
+    cur.close()
+    return 'Enter some trigger values first !'
+
+@app.route('/alerts/triggered',methods=['GET','POST'])
+def Fetching_triggered_alert():
+    cur=mysql.connection.cursor()
+    rows=cur.execute('SELECT * FROM users WHERE isTriggered="%s"'%'YES')
+    if(rows>0):
+        userDetails=cur.fetchall()
+        return render_template('triggers.html',userDetails=userDetails)
+    return 'NO value is triggered yet !'
+
+if __name__=='__main__':  
+    app.run(debug=True)
---
+++ job.py
@@ -0,0 +1,50 @@
+import schedule
+import time
+import mysql.connector
+import requests
+import smtplib
+import ssl
+
+def send_mail(receiver_email):
+    message = """ 
+    Subject: Alert Triggered for BTC !
+
+    Current price of BTC is now reached your expected value"""
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(sender_email, password)
+    s.sendmail(sender_email, receiver_email, message)
+    s.quit()
+    return
+
+def fetch_bitcoin():
+    url="https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=ma rket_cap_desc&per_page=100&page=1&sparkline=false"
+    data=requests.get(url)
+    input=data.json()
+    result = input[0]['current_price']
+    return(result)
+
+def check():
+    conn=mysql.connector.connect(host="localhost",user="root",password="ajj@7646gv",database="Kryptotask")
+    cur=conn.cursor()
+    rows=cur.execute("SELECT * FROM users WHERE isTriggered='NO'")
+    if(rows==0):return
+    else:
+        coin=fetch_bitcoin()
+        print("Current price is ",coin)
+        userDetails=cur.fetchall()
+        for user in userDetails:
+            if(user[2]==coin):
+                send_mail(user[1])
+                print("Alert Triggered: Mail sent to the respective user !")
+                cur.execute("UPDATE users SET isTriggered='YES' WHERE triggervalue=%s"%coin)
+                conn.commit()                 
+    cur.close()
+    conn.close()
+
+sender_email=input("Enter the sender email..")#"santhosh.gvelu@gmail.com"
+password=input("Enter the password...")#"jgvideimpzkfyimi"
+schedule.every(5).seconds.do(check)
+while(True):
+    schedule.run_pending()
+    time.sleep(1)
